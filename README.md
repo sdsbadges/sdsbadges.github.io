edc6212448722dd6e4bb062321c148975c8f8eea
Matt Jones <mjones@softwaredesignsolutions.com>
2019-08-10 11:16:53 -0400

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# sdsbadges.github.io
---
+++ precache-manifest.d115d54140f98ab6ee0ea2480a765519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc7a2ab8dac793431d644858d12e3d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63f467f286a6dfa81dac",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb425aac97275ca990c5",
+    "url": "/static/js/2.02a4be1c.chunk.js"
+  },
+  {
+    "revision": "63f467f286a6dfa81dac",
+    "url": "/static/js/main.98076bc7.chunk.js"
+  },
+  {
+    "revision": "db0767557cd966de1b3d",
+    "url": "/static/js/runtime~main.2e7864d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
